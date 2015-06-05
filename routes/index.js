ae80356a994119c0accb606be236da48c71bf91a
marcosjrs <marcosjrs80@gmail.com>
2015-06-05 18:38:38 +0200

    Añadido apartado con la info y el enlace autor

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,8 @@ router.get('/', function(req, res) {
 });
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', function(req ,res){
+	res.render('author',{name:'Marcos José Rivera Souto', photo:'/images/yo.png', description:'Programador backend de A Coruña.<br/>Con ganas de aprender de todo lo que se preste.'})
+});
 
 module.exports = router;
