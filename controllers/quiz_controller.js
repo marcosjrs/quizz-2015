c88d97f0045713e88f6b8b8f4287f905f16744a8
marcosjrs <marcosjrs80@gmail.com>
2015-06-10 21:20:18 +0200

    Ejercicio.Formulario de busqueda en donde lista quizes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -49,34 +49,11 @@ exports.answer = function(req, res){
 
 exports.index = function(req, res){
 	//renderiza question.ejs al que se le pasa la variable quizes con todas las preguntas
-	models.Quiz.findAll().then(function(quizes){
+	//Si el request lleva un 'parametro' "search" no vacio creamos una query similar a: 
+	//where pregunta like "%palabrabuscar%"
+	var query =  req.query.search && req.query.search != "" ? { where: { pregunta: {like:"%"+req.query.search+"%"} } } :{} ;
+	models.Quiz.findAll(query).then(function(quizes){
 		res.render('quizes/index.ejs',{ quizes:quizes });
 	}).catch(function(error){ 		next(error);		});
 };
 
-//Antes:
-/*
-//GET /quizes/question
-exports.question = function(req, res){
-	//renderiza question.ejs al que se le pasa la variable pregunta
-	// res.render('quizes/question',{pregunta:'Capital de Italia'});
-	models.Quiz.findAll().then(function(quiz){
-		res.render('quizes/question',{ pregunta:quiz[0].pregunta });
-	});
-}
-
-//GET /quizes/answer
-exports.answer = function(req, res){
-	//renderiza answer.ejs con variable respuesta dependiente de lo contestado
-	//  if(req.query.respuesta=="Roma"){ res.render('quizes/answer',{respuesta: 'Correcto'});
-	//  }else{ 						     res.render('quizes/answer',{respuesta: 'Incorrecto'});}
-	models.Quiz.findAll().then(function(quiz){
-		if(req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer',{ respuesta:'Correcto' });
-		}else{
-			res.render('quizes/answer',{ respuesta:'Incorrecto' });
-		}
-	});
-
-}
-*/
